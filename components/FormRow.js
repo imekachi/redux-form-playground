a6f28df96fc091f76557a944f795b37dc4a4174a
imekachi <imekachi@gmail.com>
2017-10-24 17:55:58 +0700

    - it's working now by importing { Element } type from react

--- components/FormRow.js
+++ components/FormRow.js
@@ -1,11 +1,12 @@
 // @flow
+import type { Element } from 'react'
 import React from 'react'
 
 type Props = {
   children?: Element<any>,
   className?: string
 }
-const FormRow = ({children, className}: Props) => (
+const FormRow = ({ children, className }: Props) => (
   <div className={'form-row ' + (className || '')}>
     {children}
   </div>
